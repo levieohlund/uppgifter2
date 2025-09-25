c2066c1f21a2b778d0c9db4ea366834582dd8711
levieohlund-lang <levie.ohlund@gmail.com>
2025-09-25 11:35:51 +0200

    uppgift

---
+++ lektion2.py
@@ -0,0 +1,12 @@
+# a) Skapa en sträng med variabler
+name = "Alice"
+age = 25
+sentence = f"Mitt namn är {name} och jag är {age} år"
+print(sentence)
+
+# b) Låt användaren skriva in en mening och skriv ut den i versaler
+user_sentence = input("Skriv en mening: ")
+print(user_sentence.upper())
+
+# c) Ta reda på längden av en sträng
+print("Längden på din mening är:", len(user_sentence))
