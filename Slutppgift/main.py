860d68f874d623fddea81827d35a68a27e3e2fac
levieohlund-lang <levie.ohlund@gmail.com>
2025-09-30 12:36:04 +0200

    "made a menu and imported psutil"

---
+++ Slutppgift/main.py
@@ -0,0 +1,11 @@
+import psutil
+import time 
+import os 
+
+def main_menu ():
+    while True: 
+        print("\nMain menu")
+        print("1. View system information")
+        print("2. list of surrveliance")
+        print("3. Quit Menu")
+        choice = input("\n Choose an option")
