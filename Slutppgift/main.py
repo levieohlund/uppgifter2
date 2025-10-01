d059cfabb264cd8e6de2a89f2d0fef8f087b85c7
levieohlund-lang <levie.ohlund@gmail.com>
2025-10-01 10:49:45 +0200

    Commit all changes including submodules

--- Slutppgift/main.py
+++ Slutppgift/main.py
@@ -7,8 +7,6 @@ def display_usage(usage):
     print(f"Memory Usage: {usage[1]}%")
     print(f"Disk Usage: {usage[2]}%")
     break
-time.sleep(5)
-    os.system('cls' if os.name == 'nt' else 'clear')
     
 while True:
     display_usage((psutil.cpu_percent(), psutil.virtual_memory().percent, 30))
